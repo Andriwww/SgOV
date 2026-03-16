5a0102d190e6a9b4938b8c58fe06129f5e51bfbb
Andriwww <114580836+Andriwww@users.noreply.github.com>
2026-03-16 23:37:51 +0100

    html

---
+++ src/main/java/es/uji/ei1027/clubesportiu/controller/UsuarioOVIController.java
@@ -0,0 +1,56 @@
+package es.uji.ei1027.clubesportiu.controller;
+
+import es.uji.ei1027.clubesportiu.dao.UsuarioOVIDao;
+import es.uji.ei1027.clubesportiu.model.UsuarioOVI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/UsuarioOVI")
+public class UsuarioOVIController {
+
+    private UsuarioOVIDao usuarioOVIDao;
+
+    @Autowired
+    public void setUsuarioOVIDao(UsuarioOVIDao usuarioOVIDao) {
+        this.usuarioOVIDao = usuarioOVIDao;
+    }
+
+    @RequestMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("usuarios", usuarioOVIDao.getUsuariosOVI());
+        return "UsuarioOVI/list";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addForm(Model model) {
+        model.addAttribute("usuario", new UsuarioOVI());
+        return "UsuarioOVI/add";
+    }
+
+    @RequestMapping(value = "/add",  method = RequestMethod.POST)
+    public String addSubmit(@ModelAttribute("usuario") UsuarioOVI usuario) {
+        usuarioOVIDao.addUsuarioOVI(usuario);
+        return "redirect:list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editForm(@PathVariable int id, Model model) {
+        model.addAttribute("usuario", usuarioOVIDao.getUsuarioOVI(id));
+        return "UsuarioOVI/update";
+    }
+
+    @PostMapping("/edit")
+    public String editSubmit(@ModelAttribute UsuarioOVI usuario) {
+        usuarioOVIDao.updateUsuarioOVI(usuario);
+        return "redirect:/UsuarioOVI/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        usuarioOVIDao.deleteUsuarioOVI(id);
+        return "redirect:/UsuarioOVI/list";
+    }
+}
