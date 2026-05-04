993d675aafc86f8519aa41a24ba141bc086186f3
al439839 <al439839@uji.es>
2026-05-04 22:31:16 +0200

    AsistentePersonal controller + validator + htmls + css done

---
+++ src/main/java/es/uji/ei1027/clubesportiu/controller/AsistentePersonalController.java
@@ -0,0 +1,83 @@
+package es.uji.ei1027.clubesportiu.controller;
+
+import es.uji.ei1027.clubesportiu.dao.AsistentePersonalDao;
+import es.uji.ei1027.clubesportiu.model.AsistentePersonal;
+import es.uji.ei1027.clubesportiu.validator.AsistentePersonalValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/AsistentePersonal")
+public class AsistentePersonalController {
+    private AsistentePersonalDao asistentePersonalDao;
+
+    @Autowired
+    public void setAsistentePersonalDao(AsistentePersonalDao dao) {
+        this.asistentePersonalDao = dao;
+    }
+
+    // LIST
+    @RequestMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("asistentes", asistentePersonalDao.getAsistentesPersonales());
+        return "AsistentePersonal/list";
+    }
+
+    // ADD FORM
+    @RequestMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("asistente", new AsistentePersonal());
+        return "AsistentePersonal/add";
+    }
+
+    // ADD SUBMIT
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addSubmit(@ModelAttribute("asistente") AsistentePersonal asistente,
+                            BindingResult result) {
+
+        AsistentePersonalValidator validator = new AsistentePersonalValidator();
+        validator.validate(asistente, result);
+
+        if (result.hasErrors()) {
+            return "AsistentePersonal/add";
+        }
+
+        asistentePersonalDao.addAsistentePersonal(asistente);
+        return "redirect:list";
+    }
+
+    // UPDATE FORM
+    @RequestMapping("/update/{idAsistente}")
+    public String editForm(Model model, @PathVariable int idAsistente) {
+        model.addAttribute("asistente",
+                asistentePersonalDao.getAsistentePersonal(idAsistente));
+        return "AsistentePersonal/update";
+    }
+
+    // UPDATE SUBMIT
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String editSubmit(@ModelAttribute("asistente") AsistentePersonal asistente,
+                             BindingResult result) {
+
+        AsistentePersonalValidator validator = new AsistentePersonalValidator();
+        validator.validate(asistente, result);
+
+        if (result.hasErrors()) {
+            return "AsistentePersonal/update";
+        }
+
+        asistentePersonalDao.updateAsistentePersonal(asistente);
+        return "redirect:list";
+    }
+
+    // DELETE
+    @RequestMapping("/delete/{idAsistente}")
+    public String delete(@PathVariable int idAsistente) {
+        asistentePersonalDao.deleteAsistentePersonal(idAsistente);
+        return "redirect:/AsistentePersonal/list";
+    }
+    
+}
